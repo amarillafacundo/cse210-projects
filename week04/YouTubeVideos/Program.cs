8eb59ab60ed05806398d0d619118d20be4de1700
amarillafacundo <facundo.e.amarilla@gmail.com>
2025-03-28 00:32:34 -0300

    update 2.0

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,50 +5,50 @@ class Comment
     public string Name { get; set; }
     public string Text { get; set; }
 
-    // Constructor
+    
     public Comment(string name, string text)
     {
         Name = name;
         Text = text;
     }
 
-    // Method to display comment
+    
     public override string ToString()
     {
         return $"{Name}: {Text}";
     }
 }
 
-// Video class to track video details and associated comments
+
 class Video
 {
     public string Title { get; set; }
     public string Author { get; set; }
-    public int Length { get; set; } // Length in seconds
+    public int Length { get; set; } 
     public List<Comment> Comments { get; set; }
 
-    // Constructor
+    
     public Video(string title, string author, int length)
     {
         Title = title;
         Author = author;
         Length = length;
-        Comments = new List<Comment>(); // Initialize the list of comments
+        Comments = new List<Comment>(); 
     }
 
-    // Method to add a comment to the video
+    
     public void AddComment(Comment comment)
     {
         Comments.Add(comment);
     }
 
-    // Method to get the number of comments
+    
     public int GetNumberOfComments()
     {
         return Comments.Count;
     }
 
-    // Method to display video details
+    
     public void DisplayVideoDetails()
     {
         Console.WriteLine($"Title: {Title}");
@@ -57,12 +57,12 @@ class Video
         Console.WriteLine($"Number of comments: {GetNumberOfComments()}");
         Console.WriteLine("Comments:");
 
-        // Display all comments
+        
         foreach (var comment in Comments)
         {
             Console.WriteLine($"  - {comment}");
         }
-        Console.WriteLine(); // For separating videos
+        Console.WriteLine(); 
     }
 }
 
@@ -85,10 +85,10 @@ class Program
         video3.AddComment(new Comment("Eve", "Amazing content!"));
         video3.AddComment(new Comment("Frank", "Very detailed, thanks!"));
 
-        // Storing videos in a list
+        
         List<Video> videos = new List<Video> { video1, video2, video3 };
 
-        // Iterating through the list of videos and displaying their details
+        
         foreach (var video in videos)
         {
             video.DisplayVideoDetails();
