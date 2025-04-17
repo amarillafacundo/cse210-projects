1c6900a0e9d21e58843038467fab162779b04566
amarillafacundo <facundo.e.amarilla@gmail.com>
2025-04-17 20:28:51 -0300

    update week 6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 5),
+            new Rectangle("Blue", 4, 6),
+            new Circle("Green", 3)
+        };
+
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}, Area: {shape.GetArea()}");
+        }
+
+
     }
 }
---
+++ week06/Shapes/base_shapes.cs
@@ -0,0 +1,16 @@
+public class Shape
+{
+    public string Color { get; set; }
+
+
+    public Shape(string color)
+    {
+        Color = color;
+    }
+
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
